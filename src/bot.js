8847e258807d16c4cc2cad99da52ef3e43d38853
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2021-05-23 14:42:10 +0545

    basics

--- src/bot.js
+++ src/bot.js
@@ -7,31 +7,43 @@ const startTheBot = async () => {
   const trades = await TradeModal.find();
 
   for (let trade of trades) {
-    const TOKEN = new Token(ChainId.ROPSTEN, trade.address, 18, trade.token);
+    try {
+      const TOKEN = new Token(ChainId.ROPSTEN, trade.address, 18, trade.token);
 
-    const pair = await Fetcher.fetchPairData(BUSD, TOKEN, provider);
+      const pair = await Fetcher.fetchPairData(BUSD, TOKEN, provider);
 
-    const route = new Route([pair], TOKEN);
+      const route = new Route([pair], TOKEN);
 
-    const currentPrice = route.midPrice.toSignificant(6);
-    console.log(`Current ${TOKEN.symbol} Price: ${currentPrice}`);
+      const currentPrice = route.midPrice.toSignificant(6);
+      console.log(`Current ${TOKEN.symbol} Price: ${currentPrice}`);
 
-    if (trade.type === "BUY" && trade.limit > 0 && currentPrice < trade.limit) {
-      const amountUSD = parseFloat(trade.amount).toFixed(8);
-      const amount = parseFloat(trade.amount / currentPrice).toFixed(8);
-      console.log(`Start buying ${amount} ${TOKEN.symbol} (${amountUSD} USD) `);
-      await buyToken(trade, amountUSD);
-    } else if (
-      trade.type === "SELL" &&
-      trade.limit > 0 &&
-      currentPrice > trade.limit
-    ) {
-      const amountUSD = parseFloat(trade.amount).toFixed(8);
-      const amount = parseFloat(trade.amount / currentPrice).toFixed(8);
-      console.log(
-        `Start selling ${amount} ${TOKEN.symbol} (${amountUSD} USD) `
-      );
-      await sellToken(trade, amountUSD);
+      if (
+        trade.type === "BUY" &&
+        trade.limit > 0 &&
+        currentPrice < trade.limit
+      ) {
+        const amountUSD = parseFloat(trade.amount).toFixed(8);
+        const tokenAmount = parseFloat(trade.amount / currentPrice).toFixed(8);
+        console.log(
+          `Start buying ${tokenAmount} ${TOKEN.symbol} (${amountUSD} USD) `
+        );
+        await buyToken(trade, amountUSD, tokenAmount);
+      } else if (
+        trade.type === "SELL" &&
+        trade.limit > 0 &&
+        currentPrice > trade.limit
+      ) {
+        const amountUSD = parseFloat(trade.amount).toFixed(8);
+        const tokenAmount = parseFloat(trade.amount / currentPrice).toFixed(8);
+        console.log(
+          `Start selling ${tokenAmount} ${TOKEN.symbol} (${amountUSD} USD) `
+        );
+        await sellToken(trade, amountUSD, tokenAmount);
+      }
+    } catch (e) {
+      console.log(`Error on token parse! ${trade.token}: ${trade.address}`);
+      console.log(`Deleting trade...`);
+      await TradeModal.deleteOne({ id: trade.id });
     }
   }
 };
