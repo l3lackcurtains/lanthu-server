7d15f2045e677869041d602f6b0b78a8fbfb25d2
l3lackcurtains <l3lackcurtains@protonmail.com>
2021-09-27 13:14:34 -0700

    make it better

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const { router } = require("./api");
 
 app.use(bodyParser.json({ limit: "50mb" }));
