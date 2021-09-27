f9ef0112be742b3d5e6d6ed1aa0f7d44a48afb1a
l3lackcurtains <l3lackcurtains@protonmail.com>
2021-09-27 01:56:56 -0700

    API added

--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post("/tokens", async (req, res) => {
   const name = req.body.name;
   const address = req.body.address;
+  const slug = req.body.slug;
 
   if (address.length !== 42 || address.substr(0, 2) !== "0x") {
     res.json({ success: false, message: `Address wrong.` });
@@ -16,7 +17,9 @@ router.post("/tokens", async (req, res) => {
     const coinInDB = await TokenModal.findOne({ name });
 
     if (coinInDB !== null) {
+      coinInDB.name = name;
       coinInDB.address = address;
+      coinInDB.slug = slug;
       coinInDB.save();
       res.json({ success: true, message: `Token updated.` });
     } else {
@@ -29,7 +32,42 @@ router.post("/tokens", async (req, res) => {
   }
 });
 
-router.delete("/token/:name", async (req, res) => {
+router.put("/tokens/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const name = req.body.name;
+  const address = req.body.address;
+  const slug = req.body.slug;
+
+  if (address.length !== 42 || address.substr(0, 2) !== "0x") {
+    res.json({ success: false, message: `Address wrong.` });
+    return;
+  }
+
+  try {
+    const coinInDB = await TokenModal.findOne({ name });
+
+    if (coinInDB !== null) {
+      await TradeModal.updateOne(
+        { id },
+        {
+          name: name,
+          address: address,
+          slug: slug,
+        }
+      );
+      res.json({ success: true, message: `Token updated.` });
+    } else {
+      const newToken = new TokenModal({ name, address });
+      newToken.save();
+      res.json({ success: true, message: `Token added` });
+    }
+  } catch (e) {
+    res.json({ success: false, message: `Error on token add.` });
+  }
+});
+
+router.delete("/tokens/:name", async (req, res) => {
   const name = req.params.name.toUpperCase();
 
   try {
@@ -52,22 +90,36 @@ router.get("/tokens", async (req, res) => {
   }
 });
 
+router.get("/tokens/:name", async (req, res) => {
+  const name = req.params.name;
+  try {
+    const token = await TokenModal.findOne({ name });
+    res.json({ success: true, message: token });
+  } catch (e) {
+    res.json({ success: false, message: `Error on token fetch.` });
+  }
+});
+
 router.post("/trades", async (req, res) => {
-  const name = rer.body.name.toUpperCase();
+  const token = req.body.token.toString().toUpperCase();
   const limit = parseFloat(req.body.limit);
   const amount = parseFloat(req.body.amount);
   const type = req.body.type;
+  const success = req.body.success;
+  const error = req.body.error;
 
   try {
-    const coinInDB = await TokenModal.findOne({ name });
+    const coinInDB = await TokenModal.findOne({ name: token });
     if (coinInDB !== null) {
       const newTrade = TradeModal({
         id: Math.floor(Math.random() * 99999),
         type: type,
         address: coinInDB.address,
-        token: name,
+        token: token,
         amount: amount,
         limit: limit,
+        success: success,
+        error: error,
       });
       newTrade.save();
       res.json({ success: true, message: `${type}: trade added.` });
@@ -75,6 +127,40 @@ router.post("/trades", async (req, res) => {
       res.json({ success: false, message: "Set the coin first." });
     }
   } catch (e) {
+    res.json({ success: false, message: "`Error on token buy." });
+  }
+});
+
+router.put("/trades/:id", async (req, res) => {
+  const id = req.params.id;
+  const token = req.body.token.toString().toUpperCase();
+  const limit = parseFloat(req.body.limit);
+  const amount = parseFloat(req.body.amount);
+  const type = req.body.type;
+  const success = req.body.success;
+  const error = req.body.error;
+
+  try {
+    const coinInDB = await TokenModal.findOne({ name: token });
+    if (coinInDB !== null) {
+      await TradeModal.updateOne(
+        { id },
+        {
+          token: token,
+          limit: limit,
+          amount: amount,
+          type: type,
+          success: success,
+          error: error,
+        }
+      );
+
+      res.json({ success: true, message: `${type}: trade added.` });
+    } else {
+      res.json({ success: false, message: "Set the coin first." });
+    }
+  } catch (e) {
+    console.log(e);
     res.json({ success: false, message: "`Error on token buy." });
   }
 });
@@ -88,9 +174,9 @@ router.get("/trades", async (req, res) => {
   }
 });
 
-router.delete("/trade/:id", async (req, res) => {
+router.delete("/trades/:id", async (req, res) => {
   const id = req.params.id;
-
+  console.log(id);
   try {
     const tradeInDB = await TradeModal.findOne({ id });
     if (tradeInDB !== null) {
