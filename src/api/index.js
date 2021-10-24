b5f9932c7c0352f4c625439d69131d966c221f77
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2021-10-23 20:02:07 -0700

    api for devices updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 
-const { TokenModal, DeviceModal, LogModal } = require('../utils/db')
+const { DeviceModal } = require('../utils/db')
 
 const router = express.Router()
 
@@ -26,58 +26,6 @@ router.get('/devices/:token', async (req, res) => {
         res.json({ success: true, message: device })
     } catch (e) {
         res.json({ success: false, message: `Error on device fetch.` })
-    }
-})
-
-router.get('/tokeninfo/:name', async (req, res) => {
-    const name = req.params.name.toUpperCase()
-
-    try {
-        const token = await TokenModal.findOne({ name })
-
-        if (token !== null) {
-            const { balance, price, bnbBalance, bnbPrice, busdBalance } =
-                await getTokenPriceAndBalance(token)
-
-            const data = {
-                token: token.name,
-                address: token.address,
-                balance,
-                bnbBalance,
-                busdBalance,
-                price,
-                bnbPrice,
-            }
-
-            res.json({ success: true, message: data })
-        } else {
-            res.json({ success: false, message: 'No token.' })
-        }
-    } catch (e) {
-        console.log(e)
-        res.json({ success: false, message: `Error on token fetch.` })
-    }
-})
-
-router.delete('/logs/:id', async (req, res) => {
-    const id = req.params.id.toString()
-    try {
-        const logInDB = await LogModal.findOne({ _id: id })
-        if (logInDB !== null) {
-            await LogModal.deleteOne({ _id: id })
-            res.json({ success: true, message: `Log removed` })
-        }
-    } catch (e) {
-        res.json({ success: false, message: `Error on token remove.` })
-    }
-})
-
-router.get('/logs', async (req, res) => {
-    try {
-        const logs = await LogModal.find().sort({ updatedAt: -1 })
-        res.json({ success: true, message: logs })
-    } catch (e) {
-        res.json({ success: false, message: `Error on logs fetch.` })
     }
 })
 
