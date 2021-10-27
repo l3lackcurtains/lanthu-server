1d6d57aaba8b5e06627885f0def90618a6b1a1fc
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2021-10-27 10:40:30 -0700

    BSC link moved to socket

--- src/graphql/token/query.resolver.js
+++ src/graphql/token/query.resolver.js
@@ -33,8 +33,7 @@ export default {
                             })
                         )
                     } catch (e) {
-                        console.log(e)
-                        console.log('Token info error.')
+                        console.error('Token info error.')
                     }
                 }
                 return { result: tokens }
